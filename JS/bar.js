a59930dea9ace48defc65ce026a0230740828b7d
Bl@ck_H00d <31480086+vhtc@users.noreply.github.com>
2022-03-18 23:38:49 -0300

    add showbar softwares

--- JS/bar.js
+++ JS/bar.js
@@ -1,10 +1,44 @@
+function animation_bars_softwares(){
+  setTimeout(() => { move1(); }, 0); 
+  setTimeout(() => { move2(); }, 500); 
+  setTimeout(() => { move3(); }, 1000); 
+  setTimeout(() => { move4(); }, 1500);
+  setTimeout(() => { move5(); }, 2000); 
+  setTimeout(() => { move6(); }, 2500);
+  setTimeout(() => { move7(); }, 3000); 
+  setTimeout(() => { move8(); }, 3500);
+  setTimeout(() => { move9(); }, 4000); 
+  setTimeout(() => { move10(); }, 4500);
+  setTimeout(() => { move11(); }, 5000); 
+  setTimeout(() => { move12(); }, 5500);
+  setTimeout(() => { move13(); }, 6000); 
+  setTimeout(() => { move14(); }, 6500);
+}
+
+//animação
+var x =0; //global x para div tela 
+
+$(window).on("scroll load",function(){
+	var aparecer = 50; // porcentagem (neste caso, é a metade, 50%)
+	var eleHeight = $('#softwares').outerHeight(); // altura da div
+	var eleTopo = $('#softwares').offset().top; // distancia da div ao topo do documento
+	var scrlTopo = $(window).scrollTop(); // quanto foi rolada a janela
+	var distance = eleTopo-scrlTopo; // distancia da div ao topo da janela
+	var altJanela = window.innerHeight; // altura da janela
+	if((x==0) && distance <= altJanela-(eleHeight*(aparecer/100)))  {
+    animation_bars_softwares(); 
+    x++;
+    
+	}
+});
+//fim animação
 
 function move1() {
-    var elem = document.getElementById("myBar");   
+    var elem = document.getElementById("bar1");   
     var width = 1;
     var id = setInterval(frame, 10);
     function frame() {
-      if (width >= 50) {
+      if (width >= 95) {
         clearInterval(id);
       } else {
         width++; 
@@ -13,11 +47,11 @@ function move1() {
     }
   }
   function move2() {
-    var elem = document.getElementById("myBar2");   
+    var elem = document.getElementById("bar2");   
     var width = 1;
     var id = setInterval(frame, 10);
     function frame() {
-      if (width >= 50) {
+      if (width >= 75) {
         clearInterval(id);
       } else {
         width++; 
@@ -26,11 +60,11 @@ function move1() {
     }
   }
   function move3() {
-    var elem = document.getElementById("myBar");   
+    var elem = document.getElementById("bar3");   
     var width = 1;
     var id = setInterval(frame, 10);
     function frame() {
-      if (width >= 50) {
+      if (width >= 100) {
         clearInterval(id);
       } else {
         width++; 
@@ -39,11 +73,11 @@ function move1() {
     }
   }
   function move4() {
-    var elem = document.getElementById("myBar2");   
+    var elem = document.getElementById("bar4");   
     var width = 1;
     var id = setInterval(frame, 10);
     function frame() {
-      if (width >= 50) {
+      if (width >= 100) {
         clearInterval(id);
       } else {
         width++; 
@@ -52,11 +86,11 @@ function move1() {
     }
   }
   function move5() {
-    var elem = document.getElementById("myBar");   
+    var elem = document.getElementById("bar5");   
     var width = 1;
     var id = setInterval(frame, 10);
     function frame() {
-      if (width >= 50) {
+      if (width >= 85) {
         clearInterval(id);
       } else {
         width++; 
@@ -65,7 +99,7 @@ function move1() {
     }
   }
   function move6() {
-    var elem = document.getElementById("myBar2");   
+    var elem = document.getElementById("bar6");   
     var width = 1;
     var id = setInterval(frame, 10);
     function frame() {
@@ -77,12 +111,12 @@ function move1() {
       }
     }
   }
-  function mov7() {
-    var elem = document.getElementById("myBar");   
+  function move7() {
+    var elem = document.getElementById("bar7");   
     var width = 1;
     var id = setInterval(frame, 10);
     function frame() {
-      if (width >= 50) {
+      if (width >= 75) {
         clearInterval(id);
       } else {
         width++; 
@@ -91,11 +125,11 @@ function move1() {
     }
   }
   function move8() {
-    var elem = document.getElementById("myBar2");   
+    var elem = document.getElementById("bar8");   
     var width = 1;
     var id = setInterval(frame, 10);
     function frame() {
-      if (width >= 50) {
+      if (width >= 85) {
         clearInterval(id);
       } else {
         width++; 
@@ -103,12 +137,12 @@ function move1() {
       }
     }
   }
-  function mov9() {
-    var elem = document.getElementById("myBar");   
+  function move9() {
+    var elem = document.getElementById("bar9");   
     var width = 1;
     var id = setInterval(frame, 10);
     function frame() {
-      if (width >= 50) {
+      if (width >= 75) {
         clearInterval(id);
       } else {
         width++; 
@@ -117,11 +151,11 @@ function move1() {
     }
   }
   function move10() {
-    var elem = document.getElementById("myBar2");   
+    var elem = document.getElementById("bar10");   
     var width = 1;
     var id = setInterval(frame, 10);
     function frame() {
-      if (width >= 50) {
+      if (width >= 100) {
         clearInterval(id);
       } else {
         width++; 
@@ -130,11 +164,11 @@ function move1() {
     }
   }
   function move11() {
-    var elem = document.getElementById("myBar");   
+    var elem = document.getElementById("bar11");   
     var width = 1;
     var id = setInterval(frame, 10);
     function frame() {
-      if (width >= 50) {
+      if (width >= 85) {
         clearInterval(id);
       } else {
         width++; 
@@ -143,7 +177,7 @@ function move1() {
     }
   }
   function move12() {
-    var elem = document.getElementById("myBar2");   
+    var elem = document.getElementById("bar12");   
     var width = 1;
     var id = setInterval(frame, 10);
     function frame() {
@@ -156,11 +190,11 @@ function move1() {
     }
   }
   function move13() {
-    var elem = document.getElementById("myBar");   
+    var elem = document.getElementById("bar13");   
     var width = 1;
     var id = setInterval(frame, 10);
     function frame() {
-      if (width >= 50) {
+      if (width >= 75) {
         clearInterval(id);
       } else {
         width++; 
@@ -169,11 +203,11 @@ function move1() {
     }
   }
   function move14() {
-    var elem = document.getElementById("myBar2");   
+    var elem = document.getElementById("bar14");   
     var width = 1;
     var id = setInterval(frame, 10);
     function frame() {
-      if (width >= 50) {
+      if (width >= 80) {
         clearInterval(id);
       } else {
         width++; 
